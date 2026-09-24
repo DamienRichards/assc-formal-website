da479421c477b707fb9dc5c083efc3ebe9f34ac2
DamienRichards <damienrichards2005@gmail.com>
2026-09-24 08:54:21 +0200

    Add HTML wrapper and HeadContent for styling

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,10 +1,25 @@
-import { createRootRoute, Outlet } from '@tanstack/react-router'
-import '../routes/styles.css' // assuming there's a global css
+import { createRootRoute, Outlet, ScrollRestoration } from '@tanstack/react-router'
+import { HeadContent, Scripts } from '@tanstack/react-router'
+import './styles.css'
 
 export const Route = createRootRoute({
-  component: () => (
-    <>
-      <Outlet />
-    </>
-  ),
+  component: RootComponent,
 })
+
+function RootComponent() {
+  return (
+    <html lang="en">
+      <head>
+        <meta charSet="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>ASSC Formal Website</title>
+        <HeadContent />
+      </head>
+      <body>
+        <Outlet />
+        <ScrollRestoration />
+        <Scripts />
+      </body>
+    </html>
+  )
+}
